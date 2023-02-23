dfb1750f5a7f4b1b406721cc053f9331827c6c6b
taka <takdanielu@yahoo.co.jp>
2023-02-23 17:31:46 +0900

    プロジェクト ファイルを追加します。

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // 何らかの処理
+        }
+
+        private void ButtonClick1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick11(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
